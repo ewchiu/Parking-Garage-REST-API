b3988813cec6e7c4e6646dd0054cac3503a9b777
Eric Chiu <ericwchiu@gmail.com>
2021-11-20 16:15:58 -0800

    added endpoint to get user by id

--- user.py
+++ user.py
@@ -19,3 +19,49 @@ def get_users():
 
         return jsonify(results), 200
 
+    else:
+        return 'Method not recognized'
+
+# get user by id
+@bp.route('/<user_id>', methods=['GET'])
+def get_user_by_id(user_id):
+
+    if request.method == 'GET':
+        sub = JWT_verify()
+
+        if sub is False:
+            return jsonify({'Error': 'JWT could not be verified'}), 401
+        elif sub is None:
+            return jsonify({'Error': 'No JWT was provided'}), 401
+        elif sub != user_id:
+            return jsonify({'Error': 'You do not have access to this user info.'}), 401
+
+        user_key = client.key('users', int(user_id))
+        user = client.get(key=user_key)
+
+        if not user:
+            error = {"Error": "No user with this user_id exists"}
+            return jsonify(error), 404
+
+        user['id'] = user.key.id
+        user['self'] = f'{request.url}/{user.key.id}'
+        return jsonify(user), 200
+
+def JWT_verify():
+    req = requests.Request()
+    jwt = request.headers.get('Authorization')
+
+    if jwt:
+        req = requests.Request()
+        jwt = jwt.split(" ")[1]
+
+        try:
+            sub = id_token.verify_oauth2_token(jwt, req, session['credentials']['client_id'])
+            sub = sub['sub']
+        except:
+            return False
+
+    else:
+        return None
+    
+    return sub
