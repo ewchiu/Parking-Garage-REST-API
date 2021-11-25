bf1a7417d3bc547c433b0707ef264adcdb4203fc
Eric Chiu <ericwchiu@gmail.com>
2021-11-24 23:55:00 -0800

    removed get user by id endpoint

--- user.py
+++ user.py
@@ -19,7 +19,6 @@ def get_users():
 
 		for user in results:
 			user['id'] = user.key.id
-			user['self'] = f'{request.url}/{user.key.id}'
 
 		return jsonify(results), 200
 
@@ -28,37 +27,3 @@ def get_users():
 		res.headers.set('Allow', ['GET'])
 		res.status_code = 405
 		return res
-
-# get user by id
-@bp.route('/<user_id>', methods=['GET'])
-def get_user_by_id(user_id):
-
-	if request.method == 'GET':
-		sub = verify()
-		user_key = client.key('users', int(user_id))
-		user = client.get(key=user_key)
-
-		if 'application/json' not in request.accept_mimetypes:
-			error = {"Error": "Not Acceptable"}
-			return jsonify(error), 406
-		elif sub is False:
-			return jsonify({'Error': 'JWT could not be verified'}), 401
-		elif sub is None:
-			return jsonify({'Error': 'No JWT was provided'}), 401
-		elif sub != user['sub']:
-			return jsonify({'Error': 'You do not have access to this user info.'}), 403
-
-		if not user:
-			error = {"Error": "No user with this user_id exists"}
-			return jsonify(error), 404
-
-		user['id'] = user.key.id
-		user['self'] = f'{request.url}/{user.key.id}'
-		return jsonify(user), 200
-
-	else:
-		res = make_response({"Error": "Method not recognized"})
-		res.headers.set('Allow', ['GET'])
-		res.status_code = 405
-		return res
-
